84d9111d6bd1b5d07949aec434b12e00c64b8cf8
meydodev <meydodev@gmail.com>
2025-09-29 19:35:59 +0200

    fourth commit

--- dist/db/db.js
+++ dist/db/db.js
@@ -5,9 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql2_1 = __importDefault(require("mysql2"));
 const connection = mysql2_1.default.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'gestiona_taxi',
+    host: 'sql7.freesqldatabase.com',
+    user: 'sql7800628',
+    port: 3306,
+    password: 'EnXv6pfTpj',
+    database: 'sql7800628',
 });
 exports.default = connection;
